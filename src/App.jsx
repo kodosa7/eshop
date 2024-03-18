0d7794aa11f496136c6d8445cab654087b88faf4
ELS <57393100+kodosa7@users.noreply.github.com>
2024-03-18 09:01:43 +0100

    Set up files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+        <>
+            <div className="container">
+                <Header />
+                <Cart />
+            </div>
+        </>
+    )
+}
+
+export default App;
