20646cdb872fa9afac8587f0188bd27e4131d943
ELS <57393100+kodosa7@users.noreply.github.com>
2024-03-20 08:48:05 +0100

    Fixed column centering

--- src/components/ShoppingPage/Cart.jsx
+++ src/components/ShoppingPage/Cart.jsx
@@ -4,15 +4,14 @@ import data from "./data.json";
 const Cart = ({ selectedItems }) => {
     const [total, setTotal] = useState(0);
 
-    useEffect( () => {
+    useEffect(() => {
         let totalPrice = 0;
-        selectedItems.forEach(itemId => {
-            const selectedItem = data.find(item => item.id === itemId);
+        selectedItems.forEach((itemId) => {
+            const selectedItem = data.find((item) => item.id === itemId);
             totalPrice += selectedItem.price;
         });
         setTotal(totalPrice);
     }, [selectedItems]);
-
 
     if (selectedItems.length === 0) {
         return (
@@ -20,48 +19,39 @@ const Cart = ({ selectedItems }) => {
                 <h2>Cart</h2>
                 <p>Add something to your cart, make it from your heart.</p>
             </>
-        )
+        );
     } else {
         return (
             <>
                 <h2>Cart</h2>
-                
-                    {selectedItems.map((itemId, index) => {
-                        const selectedItem = data.find(item => item.id === itemId);
-                        const { name, price } = selectedItem;
 
-                        return (
-                            <>
-                                <div className="row">
-                                    <div
-                                        className="col"
-                                        key={index}
-                                    >
-                                        {name}
-                                    </div>
-                                    <div className="col">
-                                        {price} $
-                                    </div>
-                                    <div className="col">
-                                        <button className="btn btn-outline-dark btn-sm">
-                                            Remove from cart
-                                        </button>
-                                    </div>
+                {selectedItems.map((itemId, index) => {
+                    const selectedItem = data.find((item) => item.id === itemId);
+                    const { name, price } = selectedItem;
+
+                    return (
+                        <>
+                            <div className="row mb-1">
+                                <div className="col-2" key={index}>
+                                    {name}
                                 </div>
-                            </>
-                        )}
-                    )}
-                
+                                <div className="col-1">{price} $</div>
+                                <div className="col-auto">
+                                    <button type="button" className="btn btn-outline-dark btn-sm">
+                                        Remove from cart
+                                    </button>
+                                </div>
+                            </div>
+                        </>
+                    );
+                })}
+
                 <div className="row">
-                    <div className="col">
-                        Total
-                    </div>
-                    <div className="col">
-                        {total} $
-                    </div>
+                    <div className="col-2">Total</div>
+                    <div className="col-1">{total} $</div>
                 </div>
                 <div className="mt-3 mb-4">
-                    <button className="btn btn-primary">
+                    <button type="button" className="btn btn-primary">
                         Checkout
                     </button>
                 </div>
--- src/components/ShoppingPage/Item.jsx
+++ src/components/ShoppingPage/Item.jsx
@@ -1,24 +1,22 @@
 const Item = (props) => {
     const { id, name, description, image, price, handleAddToCart } = props;
 
-    const handleClick = () => {
+    const handleClick = (e) => {
         handleAddToCart(id);
-    }
+        e.preventDefault();
+    };
 
     return (
         <div className="card h-100">
-            <h3 className="card-header">{ name }</h3>
-            <img className="card-img rounded-0" src={ image } alt="" />
+            <h3 className="card-header">{name}</h3>
+            <img className="card-img rounded-0" src={image} alt="" />
             <div className="card-body">
-                <p className="card-text">{ description }</p>
+                <p className="card-text">{description}</p>
             </div>
             <div className="card-footer">
                 <div className="d-flex justify-content-between align-items-center text-wrap">
-                    { price } $
-                    <button
-                        className="btn btn-outline-dark"
-                        onClick={handleClick}
-                    >
+                    {price} $
+                    <button type="button" className="btn btn-outline-dark" onClick={handleClick}>
                         Add to cart
                     </button>
                 </div>
