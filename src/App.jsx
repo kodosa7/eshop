061922985af28e212f71f0d7f2388db77b4e7082
ELS <57393100+kodosa7@users.noreply.github.com>
2024-04-17 08:04:35 +0200

    Added Footer template component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Shop from "./components/Shop"
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
         <div className="container">
             <Header />
             <Shop />
+            <Footer />
         </div>
     )
 }
