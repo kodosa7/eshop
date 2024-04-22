ffab133eca9156d7d0de4017839082d1337360f4
ELS <57393100+kodosa7@users.noreply.github.com>
2024-04-22 08:42:19 +0200

    Fetch data from API and display some of them

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Item from "./Shop/Item";
-import data from "./Shop/data.json";
 import Cart from "./Shop/Cart";
 
 const Shop = () => {
     const [selectedItems, setSelectedItems] = useState([]);
     const [disabledButtons, setDisabledButtons] = useState([]);
     const [showOrderSent, setShowOrderSent] = useState(false);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then((res) => res.json())
+            .then((data) => setData(data));
+    }, []);
+
+    const showNextItems = () => {
+        console.log("show next items btn clicked");
+    };
 
     // Handling whether the item is or isn't in cart
     const handleAddToCart = (itemId) => {
@@ -49,22 +59,31 @@ const Shop = () => {
             <div className="row">
                 <h2>Articles (goods)</h2>
                 {/* Item */}
-                <div className="row row-gap-4">
-                    {data.map((item, index) => (
-                        <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
-                            <Item
-                                itemId={item.id}
-                                index={index}
-                                name={item.name}
-                                image={item.image}
-                                description={item.description}
-                                price={item.price}
-                                handleAddToCart={() => handleAddToCart(item.id)}
-                                disabledButtons={disabledButtons}
-                            />
-                        </div>
-                    ))}
-                </div>
+
+                {/* show only if data.products is not empty - not when initial state = [] */}
+                {data.products && (
+                    <div className="row row-gap-4">
+                        {data.products.map((item, index) => (
+                            <div key={item.id} className="col-sm-6 col-md-4 col-lg-3">
+                                <Item
+                                    itemId={item.id}
+                                    index={index}
+                                    name={item.title}
+                                    image={item.thumbnail}
+                                    description={item.description}
+                                    price={item.price}
+                                    handleAddToCart={() => handleAddToCart(item.id)}
+                                    disabledButtons={disabledButtons}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            <div className="row">
+                <button className="btn btn-primary" onClick={showNextItems}>
+                    Next 20 items
+                </button>
             </div>
         </>
     );
