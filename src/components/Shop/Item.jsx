01f3b9ba8d72409a56232dee83cca0e4fe2c13e8
ELS <57393100+kodosa7@users.noreply.github.com>
2024-05-07 14:21:00 +0200

    Removed font styling; Moved original price next to the discounted price and changed font weight and font size

--- src/components/Shop/Item.jsx
+++ src/components/Shop/Item.jsx
@@ -20,14 +20,11 @@ const Item = (props) => {
             </div>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">{starsArray}</li>
-                <li className="list-group-item fw-light">
-                    <s>was ${price}</s>
-                </li>
             </ul>
 
             <div className="card-footer">
                 <div className="d-flex justify-content-between align-items-center text-wrap">
-                    <div className="fw-bold">
+                    <div className="fw-bold fs-4">
                         $
                         {discountPrice.toFixed(2).split(".")[1] === "00"
                             ? discountPrice.toFixed(2).split(".")[0]
@@ -36,17 +33,17 @@ const Item = (props) => {
                                   .split(".")[1]
                                   .padEnd(2, "0")}`}{" "}
                     </div>
+                    <div className="fw-light fs-6">
+                        <s>${price}</s>
+                    </div>
                     <button
                         key={itemId}
                         type="button"
                         className="btn btn-outline-dark"
-                        style={{
-                            fontSize: ".7rem",
-                        }}
                         onClick={() => handleAddToCart(item)}
                         disabled={disabledButtons.includes(itemId)}
                     >
-                        {disabledButtons.includes(itemId) ? `Already in your cart` : `Add to cart`}
+                        {disabledButtons.includes(itemId) ? "Already in cart" : "Buy"}
                     </button>
                 </div>
             </div>
