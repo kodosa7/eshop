a9f42969b8a5e1a12bcf8ba2968b18f9c19f8baf
ELS <57393100+kodosa7@users.noreply.github.com>
2024-05-09 08:40:32 +0200

    Removed clickable feature on rating

--- src/components/Shop/Rating.jsx
+++ src/components/Shop/Rating.jsx
@@ -1,37 +1,20 @@
-import { useState, useEffect } from "react";
 import { FaStar } from "react-icons/fa";
 
 const Rating = (props) => {
-    const { item, setRatingOnClick, myRating, setMyRating } = props;
-    const [hover, setHover] = useState(null);
-
-    useEffect(() => {
-        setMyRating(item.rating);
-    }, [item.rating]);
+    const { myRating } = props;
 
     return (
         <div className="">
             {[...Array(5)].map((star, index) => {
                 const currentRating = index + 1;
                 return (
-                    <label key={index}>
-                        <input
-                            type="radio"
-                            name="rating"
-                            value={currentRating}
-                            onClick={() => setRatingOnClick(currentRating, index)}
-                            style={{ display: "none" }}
-                        />
-                        <FaStar
-                            style={{
-                                cursor: "pointer",
-                                color: currentRating <= (hover || myRating) ? "#ffc107" : "#e4e5e9",
-                            }}
-                            size={25}
-                            onMouseEnter={() => setHover(currentRating)}
-                            onMouseLeave={() => setHover(null)}
-                        />
-                    </label>
+                    <FaStar
+                        key={index}
+                        style={{
+                            color: currentRating <= myRating ? "#ffc107" : "#e4e5e9",
+                        }}
+                        size={25}
+                    />
                 );
             })}
         </div>
