9a9a4bff20cf4ac53d736510170eb1b357c6d90e
ELS <57393100+kodosa7@users.noreply.github.com>
2024-05-09 09:41:42 +0200

    Half star display over grey stars

--- src/components/Shop/Rating.jsx
+++ src/components/Shop/Rating.jsx
@@ -1,4 +1,4 @@
-import { FaStar } from "react-icons/fa";
+import { FaStar, FaStarHalf } from "react-icons/fa";
 
 const Rating = (props) => {
     const { rating } = props;
@@ -7,14 +7,28 @@ const Rating = (props) => {
         <div className="">
             {[...Array(5)].map((star, index) => {
                 const currentRating = index + 1;
+                let newStar;
+
+                if (currentRating <= rating) {
+                    newStar = <FaStar style={{ color: "#ffc107" }} size={25} />;
+                } else if (currentRating - rating < 0.5) {
+                    newStar = (
+                        <>
+                            <FaStar style={{ color: "#e4e5e9" }} size={25} />
+                            <FaStarHalf
+                                style={{ color: "#ffc107", position: "absolute", left: 0, top: "1px" }}
+                                size={25}
+                            />
+                        </>
+                    );
+                } else {
+                    newStar = <FaStar style={{ color: "#e4e5e9" }} size={25} />;
+                }
+
                 return (
-                    <FaStar
-                        key={index}
-                        style={{
-                            color: currentRating <= rating ? "#ffc107" : "#e4e5e9",
-                        }}
-                        size={25}
-                    />
+                    <span key={index} style={{ position: "relative" }}>
+                        {newStar}
+                    </span>
                 );
             })}
         </div>
