afca8393859181c8f856c0765aac9d03642cd88b
ELS <iam.treeleafgirl@gmail.com>
2024-08-26 13:26:03 +0200

    Styles moved to index.css file, linked in HTML; Made a gap between title and subtitle.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import logo from "/src/assets/logo.svg";
 
 const Header = () => {
     return (
-        <div className="d-flex flex-row" style={{border: '1px solid red'}}>
+        <div className="d-flex flex-row">
             <div className="logo">
                 <img src={ logo } alt="Logo" className="logo" />
             </div>
-            <div className="title d-flex mt-2 align-items-end" style={{border: '1px solid blue'}}>MIRAMALL</div>
-            <div className="subtitle d-flex align-items-end" style={{border: '1px solid green'}}>Where Retail Dreams Come True</div>
+            <div className="title d-flex mt-2 align-items-end">MIRAMALL</div>
+            <div className="title-gap"></div>
+            <div className="subtitle d-flex align-items-end">Where Retail Dreams Come True</div>
         </div>
     )
 }
