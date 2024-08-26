d4895ac5b23292618f9fd9896d9d40dc0a0fd571
ELS <iam.treeleafgirl@gmail.com>
2024-08-26 14:25:20 +0200

    Header styling; New font.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,26 @@ import logo from "/src/assets/logo.svg";
 
 const Header = () => {
     return (
-        <div className="d-flex flex-row">
-            <div className="logo">
-                <img src={ logo } alt="Logo" className="logo" />
+        <>
+            <div className="mt-2 mb-2">
+                <div className="logo">
+                    <img src={ logo } alt="Logo" className="logo" />
+                </div>
             </div>
-            <div className="title d-flex mt-2 align-items-end">MIRAMALL</div>
-            <div className="title-gap"></div>
-            <div className="subtitle d-flex align-items-end">Where Retail Dreams Come True</div>
-        </div>
+            <div className="row mt-3 mb-3">
+                <div className="col-sm-2">
+                    <div className="row">
+                        <div className="title-first">MIRA</div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="row">
+                        <div className="title-second">&nbsp;</div>
+                        <div className="title-second">MALL</div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
