fd0252cfe1d7f610723c69657a4f3e70d2dbace7
ELS <iam.treeleafgirl@gmail.com>
2024-08-26 11:31:52 +0200

    Added Header subtitle.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <>
-            <div className="title">MIRAMALL</div>
-            <div className="subtitle">WHERE RETAIL DREAMS COME TRUE</div>
-        </>
+        <div className="d-flex flex-row" style={{border: '1px solid red'}}>
+            <div className="title" style={{border: '1px solid blue'}}>MIRasdasdsAMALL</div>
+            <div className="subtitle" style={{border: '1px solid green'}}>Where Retail Dreams Come Truessss</div>
+        </div>
     )
 }
 
