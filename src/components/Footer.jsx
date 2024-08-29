cff3f4493ed21e7624e3394e14430b52874380e2
ELS <iam.treeleafgirl@gmail.com>
2024-08-29 10:06:47 +0200

    Changed to col-md-2 for footer cols; Removed media query for small font.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <div className="col-12 col-md-3 mb-3 mb-md-0 text-center text-md-start">
                 <img src={ logo } alt="Logo" className="img-fluid" style={{ maxHeight: "50px", position: "relative", zIndex: 2 }} />
             </div>
-            <div className="col-2">
+            <div className="col-md-2">
                 <h6>Shipping</h6>
                 <ul>
                     <li><a href="#">Delivery Options</a></li>
@@ -21,21 +21,21 @@ const Footer = () => {
                     <li><a href="#">Fraud and Security</a></li>
                 </ul>
             </div>
-            <div className="col-2">
+            <div className="col-md-2">
                 <h6>Orders</h6>
                 <ul>
                     <li><a href="#">How to Buy</a></li>
                     <li><a href="#">Claims</a></li>
                 </ul>
             </div>
-            <div className="col-2">
+            <div className="col-md-2">
                 <h6>About us</h6>
                 <ul>
                     <li><a href="#">Contact</a></li>
                     <li><a href="#">Compliance</a></li>
                 </ul>
             </div>
-            <div className="col-3">
+            <div className="col-md-2">
                 <h6>Follow us</h6>
                 <a href="#"><i className="bi bi-facebook fs-2 pe-3"></i></a>
                 <a href="#"><i className="bi bi-instagram fs-2 pe-3"></i></a>
